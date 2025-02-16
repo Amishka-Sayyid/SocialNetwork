5f6bafd58e777eb967f0ac5d36bdf407a3f6d0f0
Amina Said Ibrahim <amina7585@gmail.com>
2025-02-16 21:28:28 +0000

    function to delete user profile

---
+++ src/app/profile/[id]/delete-profile/page.js
@@ -0,0 +1,43 @@
+import { db } from "@/utils/dbConnection";
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
+import Link from "next/link";
+
+export default async function DeleteUser({ params }) {
+  const slug = params;
+
+  async function handleDelete() {
+    "use server";
+
+    await db.query(`DELETE FROM userprofile WHERE user_id = $1`, [slug.id]);
+
+    revalidatePath("/");
+    redirect("/");
+  }
+  return (
+    <>
+      <div className="flex justify-center flex-col items-center w-full sm:w-[500px] max-w-lg p-8  rounded-lg shadow-lg bg-white">
+        <h1>are you sure you want to delete your account?</h1>
+        <h2>To proceed, please click the button below.</h2>
+        <form action={handleDelete}>
+          <button
+            type="submit"
+            className="flex hover:bg-red-600 h-8 hover:text-white bg-red-100 p-1 rounded text-black items-center"
+          >
+            Delete
+          </button>
+        </form>
+
+        <h3 className="mb-4">
+          or Click below to navigate back to your profile page
+        </h3>
+        <Link
+          href={`/profile`}
+          className="text-black-500 hover:bg-blue-500 w-full mt-6 p-1 rounded-md border-2 bg-blue-200 text-center"
+        >
+          Go to Profile
+        </Link>
+      </div>
+    </>
+  );
+}
