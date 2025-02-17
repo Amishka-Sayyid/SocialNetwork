73723cf1d2e7457dfd1539ef63121e74009d33b6
Amina Said Ibrahim <amina7585@gmail.com>
2025-02-17 00:02:51 +0000

    done with posts form and successful fetching from db

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -7,7 +7,7 @@ export default async function Profile() {
   const id = userId;
 
   const user = await db.query(
-    `SELECT username FROM userprofile WHERE user_id = $1`,
+    `SELECT id, username FROM userprofile WHERE user_id = $1`,
     [id]
   );
   if (!user.rows.length) {
@@ -31,36 +31,38 @@ export default async function Profile() {
   const username = user.rows[0].username;
 
   return (
-    <div className="flex justify-center flex-col items-center w-full sm:w-[500px] max-w-lg p-8 rounded-lg shadow-lg bg-white">
-      <h1 className={bodystyles.h1}>Welcome to Your Profile, {username}!</h1>
+    <>
+      <div className="flex justify-center flex-col items-center w-full sm:w-[500px] max-w-lg p-8 rounded-lg shadow-lg bg-white">
+        <h1 className={bodystyles.h1}>Welcome to Your Profile, {username}!</h1>
 
-      <h2 className={bodystyles.h2}>Link to view your page is here</h2>
+        <h2 className={bodystyles.h2}>Link to view your page is here</h2>
 
-      <Link
-        href={`/user/${username}`}
-        className="text-black-500 hover:bg-blue-500 w-full mt-6 p-1 rounded-md border-2 bg-blue-200 text-center"
-      >
-        User Page
-      </Link>
-
-      <h2 className={bodystyles.h2}>Manage your profile settings here.</h2>
-      <nav className="flex items-center justify-center p-2 gap-3">
-        {/* Update Profile Link */}
         <Link
-          href={`/profile/${id}/update-profile`}
-          className="text-emerald-500 hover:text-blue-700 w-full mt-6 p-1 rounded-md border-2 bg-white text-center"
+          href={`/user/${username}`}
+          className="text-black-500 hover:bg-blue-500 w-full mt-6 p-1 rounded-md border-2 bg-blue-200 text-center"
         >
-          Update Profile
+          User Page
         </Link>
 
-        {/* Delete Profile Link */}
-        <Link
-          href={`/profile/${id}/delete-profile`}
-          className="w-full hover:bg-red-500 mt-6 p-1 text-gray-800 rounded-md border-2 bg-red-300"
-        >
-          Delete Profile
-        </Link>
-      </nav>
-    </div>
+        <h2 className={bodystyles.h2}>Manage your profile settings here.</h2>
+        <nav className="flex items-center justify-center p-2 gap-3">
+          {/* Update Profile Link */}
+          <Link
+            href={`/profile/${id}/update-profile`}
+            className="text-emerald-500 hover:text-blue-700 w-full mt-6 p-1 rounded-md border-2 bg-white text-center"
+          >
+            Update Profile
+          </Link>
+
+          {/* Delete Profile Link */}
+          <Link
+            href={`/profile/${id}/delete-profile`}
+            className="w-full hover:bg-red-500 mt-6 p-1 text-gray-800 rounded-md border-2 bg-red-300"
+          >
+            Delete Profile
+          </Link>
+        </nav>
+      </div>
+    </>
   );
 }
