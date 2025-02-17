d39f2948396ccb56bba534852710758c49dae9d0
Amina Said Ibrahim <amina7585@gmail.com>
2025-02-17 00:54:57 +0000

    fetched selected  user posts and displayed them on the user profile page as well as all posts on the posts page

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,48 @@
-import { auth } from "@clerk/nextjs/server";
+import bodystyles from "./body.module.css";
 import Link from "next/link";
-import { db } from "@/utils/dbConnection";
-
-export default async function PostsPage() {
-  const { userId } = await auth();
-  const id = userId;
-
-  // Get user profile
-  const user = await db.query(`SELECT id FROM userprofile WHERE user_id = $1`, [
-    id,
-  ]);
-  if (!user.rows.length) {
-    return <div>Profile not found. Please create one.</div>;
-  }
-
-  const personalid = user.rows[0].id;
-
+import userstyles from "./userprofile.module.css";
+export default function HomePage() {
   return (
     <>
-      <h1>Posts Page</h1>
-      <Link
-        href={`/posts/${personalid}`}
-        className="bg-gray-100 hover:bg-gray-300 transition-colors duration-200 p-2 rounded text-black"
+      <div
+        className={`${bodystyles.section} flex justify-center flex-col items-center w-full sm:w-[500px] md:w-[600px] lg:w-[800px] p-8 rounded-lg shadow-lg bg-white`}
       >
-        Create Post
-      </Link>
+        <h1 className={bodystyles.h1}>
+          Every Image Tells a Story — What&apos;s Yours?
+        </h1>
+        <h2 className={bodystyles.h2}>
+          Share your moments, ideas, and creativity with the world. Whether it’s
+          a snapshot of life or a masterpiece in the making, this is your space
+          to express, inspire, and connect.
+        </h2>
+      </div>
+      <div
+        className={`${userstyles.section} flex justify-center flex-col items-center w-full sm:w-[500px] md:w-[600px] lg:w-[800px] p-8 rounded-lg shadow-lg bg-white`}
+      >
+        <h1 className={`${bodystyles.h1} text-black`}>
+          Navigate Your Next Adventure
+        </h1>
+        <nav className="flex  items-center justify-center p-2 gap-3">
+          <Link
+            href="/"
+            className="bg-gray-100 hover:bg-gray-300 transition-colors duration-200 p-2 rounded text-black"
+          >
+            Homepage
+          </Link>
+          <Link
+            href="/posts"
+            className="bg-gray-100 hover:bg-gray-300 transition-colors duration-200 p-2 rounded text-black"
+          >
+            All Posts
+          </Link>
+          <Link
+            href="/profile"
+            className="bg-gray-100 hover:bg-gray-300 transition-colors duration-200 p-2 rounded text-black"
+          >
+            Profile
+          </Link>
+        </nav>
+      </div>
     </>
   );
 }
--- src/app/posts/page.js
+++ src/app/posts/page.js
@@ -2,6 +2,8 @@ import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
 import { db } from "@/utils/dbConnection";
 import Image from "next/image";
+import bodystyles from "../body.module.css";
+import userstyles from "../userprofile.module.css";
 export default async function PostsPage() {
   const { userId } = await auth();
   const id = userId;
@@ -20,8 +22,10 @@ export default async function PostsPage() {
 
   return (
     <>
-      <div className="flex justify-center flex-col items-center w-full sm:w-[500px] max-w-lg p-8 rounded-lg shadow-lg bg-white">
-        <h1>Posts Page</h1>
+      <div
+        className={` ${userstyles.section} flex justify-center flex-col items-center w-full sm:w-[500px] max-w-lg p-8 rounded-lg shadow-lg bg-white`}
+      >
+        <h1 className={`${bodystyles.h1} text-black`}>Posts Page</h1>
         <Link
           href={`/posts/${personalid}`}
           className="bg-gray-100 hover:bg-gray-300 transition-colors duration-200 p-2 rounded text-black"
@@ -31,24 +35,30 @@ export default async function PostsPage() {
       </div>
       <div className="flex justify-center flex-col items-center w-full sm:w-[500px] max-w-lg p-8 rounded-lg shadow-lg bg-white">
         <h1>all posts</h1>
-        {posts.rows.length === 0 ? (
-          <div>No posts available.</div>
-        ) : (
-          posts.rows.map((post) => (
-            <div key={post.id}>
-              <Image
-                src={
-                  post.src ||
-                  "https://www.shutterstock.com/image-vector/image-icon-trendy-flat-style-600nw-643080895.jpg"
-                }
-                alt={post.title || "No title available"}
-                width={200}
-                height={200}
-                objectFit="cover"
-              />
-            </div>
-          ))
-        )}
+        <div className="grid grid-cols-3 gap-4">
+          {posts.rows.length === 0 ? (
+            <div>No posts available.</div>
+          ) : (
+            posts.rows.map((post) => (
+              <div
+                key={post.id}
+                className="overflow-hidden rounded-lg shadow-lg"
+              >
+                <Image
+                  src={
+                    post.src ||
+                    "https://www.shutterstock.com/image-vector/image-icon-trendy-flat-style-600nw-643080895.jpg"
+                  }
+                  alt={post.title || "No title available"}
+                  width={200}
+                  height={200}
+                  style={{ objectFit: "cover" }}
+                  className="w-full h-full"
+                />
+              </div>
+            ))
+          )}
+        </div>
       </div>
     </>
   );
